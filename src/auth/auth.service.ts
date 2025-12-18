85207bcdcc7ea503507828a8ac8a49871a000051
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-12-18 22:21:49 +0200

    saved admin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,27 @@
 import { AdminDto } from "src/dto/AdminDto";
 import * as argon2 from "argon2";
+import { PrismaService } from "src/prisma/prisma.service";
 
 export class AuthService{
+    constructor(
+          private prisma: PrismaService,  
+    ){}
     async signUp(admindto:AdminDto){
     const hashedPassword=await argon2.hash(admindto.password)
+     const admin = await this.prisma.user.create({
+        data: {
+          firstName:admindto.firstName,
+          lastName:admindto.lastName,
+          email:admindto.email,
+          password:admindto.password,
+          level:admindto.level,
+          username:admindto.username,
+          nationalId:admindto.nationalId
+
+        
+        },
+      });
+
 
     }
 }
