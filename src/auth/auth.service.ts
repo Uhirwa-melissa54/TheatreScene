092376e33ac583c66ca9102d831e7e651d2747af
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-12-18 21:42:50 +0200

    added hasing of password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { AdminDto } from "src/dto/AdminDto";
+import * as argon2 from "argon2";
 
 export class AuthService{
     async signUp(admindto:AdminDto){
+    const hashedPassword=await argon2.hash(admindto.password)
 
     }
 }
