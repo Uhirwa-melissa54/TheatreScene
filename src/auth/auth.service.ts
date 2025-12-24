3491209c802dcc54f081de7827e377a20df6071f
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-12-24 23:54:27 +0200

    added login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,6 +20,38 @@ export class AuthService{
         },
       });
 
+      async login(logindto: LoginDto) {
+  // 1. find user
+  const user = await this.prisma.user.findUnique({
+    where: { email: logindto.email },
+  });
+
+  if (!user) {
+    throw new Error("Invalid credentials");
+  }
+
+  // 2. verify password
+  const passwordMatch = await argon2.verify(
+    user.password,
+    logindto.password
+  );
+
+  if (!passwordMatch) {
+    throw new Error("Invalid credentials");
+  }
+
+  // 3. return user (later → JWT)
+  return {
+    message: "Login successful",
+    user: {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+  };
+}
+
+
 
     }
 }
