2162f71e7d4c104d41daf15917d13e04759953fa
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-12-24 23:58:42 +0200

    added restriction dto

--- src/dto/LoginAdminDto.ts
+++ src/dto/LoginAdminDto.ts
@@ -1,4 +1,8 @@
+import { IsEmail, isNotEmpty, IsNotEmpty, IsOptional } from "class-validator"
 export class LoginDto {
+  @IsEmail() 
+  @IsNotEmpty() 
   email: string;
+  @IsNotEmpty()
   password: string;
 }
